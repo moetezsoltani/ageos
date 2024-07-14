510bf04f2a35e10a6b9ab44272ab4bb8ff032e0f
moetezsoltani <moetezsoltani1@gmail.com>
2024-07-14 14:56:09 +0100

    Update settings.py

--- ageos_project/settings.py
+++ ageos_project/settings.py
@@ -118,7 +118,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-STATIC_URL = '/static/'STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATIC_URL = '/static/' 
+
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.ManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Media files (uploads)
 MEDIA_URL = '/media/'
